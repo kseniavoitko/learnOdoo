fffa8a87dc9b58c25ce7d3615072952962a0041f
KV <kseniavoytko95@gmail.com>
2022-05-07 22:03:11 +0300

    academy

---
+++ academy/models/models.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+class Teachers(models.Model):
+    _name = 'academy.teachers'
+
+    name = fields.Char()
+    biography = fields.Html()
+
+    course_ids = fields.One2many('product.template', 'teacher_id', string="Courses")
+
+class Courses(models.Model):
+    # _name = 'academy.courses'
+    # _inherit = 'mail.thread'
+    _inherit = 'product.template'
+
+    # name = fields.Char()
+    teacher_id = fields.Many2one('academy.teachers', string="Teacher")
+
+
+
+
